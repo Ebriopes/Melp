0a4d6de59ddc0889dd78ccccc5aead6070fcf5ab
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-12 18:52:19 -0500

    Add background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
+import { makeStyles } from '@material-ui/core';
+
+const useStyle = makeStyles(() =>({
+  App: {
+    background: 'url("https://i.pinimg.com/originals/a9/dc/ae/a9dcae3a2c79d488aa421e435fcb7c73.jpg")',
+    backgroundSize: "cover",
+    backgroundPositionX: "center",
+    backgroundPositionY: "bottom",
+    height: "100vh",
+  }
+}))
 
 function App() {
+  const classes = useStyle();
   return (
-    <div className="App">
+    <div className={classes.App}>
+      <Navbar/>
       <header className="App-header">
         <p> Start page </p>
       </header>
