af63c428e31117c7f0861e4015b009acd7a1d8d7
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-13 04:03:31 -0500

    Changes in the style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,71 +1,78 @@
 import React from 'react';
-import { 
-	AppBar, 
-	fade, 
-	InputBase, 
-	makeStyles, 
+import { Search as SearchIcon, } 	from '@material-ui/icons';
+import {
+	AppBar,
+	fade,
+	InputBase,
+	makeStyles,
 	Toolbar,
-	Typography, 
+	Typography,
 } from '@material-ui/core';
-import { 
-	Search as SearchIcon,
-} from '@material-ui/icons';
 
-const useStyle = makeStyles((theme) => ({
-	appbar:{
-		backgroundColor: "#ff9800aa",
-		color: "smokewhite",
+const navHeight = 64;
+
+const useStyle = makeStyles( ( theme ) => ( {
+	appbar: {
+		backgroundColor: "#00bcd4aa",
+		color: "whitesmoke",
 		shadow: "none",
+		height: `${navHeight}px`,
 	},
 	search: {
 		position: 'relative',
+		alignContent: 'center',
 		borderRadius: theme.shape.borderRadius,
-		marginRight: theme.spacing(2),
+		marginRight: theme.spacing( 2 ),
 		marginLeft: 0,
 		width: '100%',
-		[theme.breakpoints.up('sm')]: {
-			marginLeft: theme.spacing(3),
+		[ theme.breakpoints.up( 'sm' ) ]: {
+			marginLeft: theme.spacing( 3 ),
 			width: 'auto',
 		},
-		backgroundColor: fade(theme.palette.common.white, 0.15),
+		backgroundColor: fade( theme.palette.common.white, 0.15 ),
 		'&:hover': {
-			backgroundColor: fade(theme.palette.common.white, 0.25),
+			backgroundColor: fade( theme.palette.common.white, 0.25 ),
 		},
 	},
 	inputRoot: {
 		color: 'inherit',
-	  },
-	  inputInput: {
-		padding: theme.spacing(1, 1, 1, 0),
+	},
+	inputInput: {
+		padding: theme.spacing( 1, 1, 1, 0 ),
 		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-		transition: theme.transitions.create('width'),
+		paddingLeft: `calc(1em + ${ theme.spacing( 4 ) }px)`,
+		transition: theme.transitions.create( 'width' ),
 		width: '100%',
-		[theme.breakpoints.up('md')]: {
+		[ theme.breakpoints.up( 'md' ) ]: {
 			width: '20ch',
 		},
 	},
-}))
+} ) );
 
-function Navbar() {
+function Navbar () {
 	const classes = useStyle();
+
 	return (
-		<AppBar className={classes.appbar} color="transparent" position="sticky">
+		<AppBar className={ classes.appbar } position="sticky">
 			<Toolbar>
-				<Typography variant="h3">
-					MELP
-				</Typography>
-				<div className={classes.search}>
-					<InputBase 
+
+				<Typography variant="h4"> MELP </Typography>
+
+				<div className={ classes.search }>
+					
+					<InputBase
 						placeholder="Search"
-						classes={{
+						classes={ {
 							root: classes.inputRoot,
 							input: classes.inputInput,
-						}}
-						inputProps={{ 'aria-label': 'search' }}
+						} }
+						inputProps={ { 'aria-label': 'search' } }
 					/>
-						<SearchIcon/>
+
+					<SearchIcon fontSize="large" />
+
 				</div>
+
 			</Toolbar>
 		</AppBar>
 	)
