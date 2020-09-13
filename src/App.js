45c74a2dfdbd87bc7cc3771dd77158ee3b0e5f02
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-13 04:05:47 -0500

    Added 'presentation' view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import { makeStyles } from '@material-ui/core';
+import React                from 'react';
+import { makeStyles }       from '@material-ui/core';
+import Navbar               from './Components/Navbar';
+import Home                 from './Views/Home';
+import Presentation         from './Views/Presentation';
+
 
 const useStyle = makeStyles(() =>({
   App: {
-    background: 'url("https://i.pinimg.com/originals/a9/dc/ae/a9dcae3a2c79d488aa421e435fcb7c73.jpg")',
+    background: 'url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fopenforbusiness.opentable.com%2Fwp-content%2Fuploads%2Fsites%2F100%2F2015%2F07%2F20141027-Wide-Towards-Bar-1.jpg&f=1&nofb=1")',
     backgroundSize: "cover",
     backgroundPositionX: "center",
     backgroundPositionY: "bottom",
@@ -17,9 +20,8 @@ function App() {
   return (
     <div className={classes.App}>
       <Navbar/>
-      <header className="App-header">
-        <p> Start page </p>
-      </header>
+      <Home/>
+      <Presentation/>
     </div>
   );
 }
