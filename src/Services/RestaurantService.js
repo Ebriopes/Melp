e9b2c16483b7772808699a8f67692f510e8406fb
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-14 13:15:58 -0500

    This service call to the api to get data about restaurants

--- src/Services/RestaurantService.js
+++ src/Services/RestaurantService.js
@@ -3,10 +3,10 @@ import Axios from 'axios';
 async function RestaurantService () {
 	try {
 		//Correct URL: https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json
-		const { data } = await Axios.get( "https://ebriopes.firebaseio.com/ebriopes.json")
+		const { data } = await Axios.get( "https://ebriopes.firebaseio.com/ebriopes.json");
 		return data;
 	} catch ( error ) {
-		console.error( error )
+		console.error( error );
 	}
 }
 
