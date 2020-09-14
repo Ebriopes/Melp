4a0e1aabbbddb9215ed8d340c9310ec8a0186b90
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-14 13:09:14 -0500

    Added background image, Navbar and Home views

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React               from 'react';
 import { makeStyles }      from '@material-ui/core';
-import Navbar              from './Components/Navbar';
 import Home                from './Views/Home';
-import Presentation        from './Views/Presentation';
-import SortContextProvider from './Contexts/SortContext';
+import Navbar              from './Components/Navbar';
+import background          from './assets/background.jpeg';
 
 const useStyle = makeStyles(() =>({
-  App: {
-    background: 'url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fopenforbusiness.opentable.com%2Fwp-content%2Fuploads%2Fsites%2F100%2F2015%2F07%2F20141027-Wide-Towards-Bar-1.jpg&f=1&nofb=1")',
-    backgroundSize: "cover",
-    backgroundPositionX: "center",
-    backgroundPositionY: "bottom",
-    height: "100vh",
-  }
+  bg: {
+    width: '100%',
+    zIndex: '-1',
+    position: 'absolute',
+    maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0))',
+  },
 }))
 
 function App() {
+  
   const classes = useStyle();
+
   return (
-    <div className={classes.App}>
-      <SortContextProvider>
+    <React.Fragment>
+      
+      <img  src={background}
+            className={classes.bg}
+            alt='background'
+            />
+
         <Navbar/>
-        {/* <Home/> */}
-        <Presentation/>
-      </SortContextProvider>
-    </div>
+        <Home/>
+    </React.Fragment>
   );
 }
 
