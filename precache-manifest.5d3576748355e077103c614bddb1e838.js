c8709ad345f21878482a8deba149d5fdbdee25f7
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-14 15:09:23 -0500

    Updates

---
+++ precache-manifest.5d3576748355e077103c614bddb1e838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8459bd92a1b159c173b1969bcc5df2e2",
+    "url": "/Melp/index.html"
+  },
+  {
+    "revision": "94fcfe01a9e8588c52cb",
+    "url": "/Melp/static/js/2.b41631f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Melp/static/js/2.b41631f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2834185bfd681d8ab90b",
+    "url": "/Melp/static/js/main.c524375d.chunk.js"
+  },
+  {
+    "revision": "eaa8b6a6c0d9ae2671af",
+    "url": "/Melp/static/js/runtime-main.46bbd13e.js"
+  },
+  {
+    "revision": "c49abdedcc716d2cb0b1133605b3eb40",
+    "url": "/Melp/static/media/background.c49abded.jpeg"
+  }
+]);
