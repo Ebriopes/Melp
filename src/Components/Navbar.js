762baff2b591a3f02ab0790a93eefe44b131f110
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-14 13:19:26 -0500

    Navbar component show the app name and two buttons to right side, set transparent background and the color to "coral"

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,51 +1,28 @@
 import React from 'react';
-import { Search as SearchIcon, } 	from '@material-ui/icons';
 import {
 	AppBar,
-	fade,
-	InputBase,
+	Button,
 	makeStyles,
 	Toolbar,
 	Typography,
 } from '@material-ui/core';
 
-const navHeight = 64;
-
-const useStyle = makeStyles( ( theme ) => ( {
+const useStyle = makeStyles( ( ) => ( {
 	appbar: {
-		backgroundColor: "#00bcd4aa",
-		color: "whitesmoke",
-		shadow: "none",
-		height: `${navHeight}px`,
+		position: 'static',
+		color: 'coral',
+		boxShadow: 'none',
+		fontFamily: 'Lobster',
 	},
-	search: {
-		position: 'relative',
-		alignContent: 'center',
-		borderRadius: theme.shape.borderRadius,
-		marginRight: theme.spacing( 2 ),
-		marginLeft: 0,
-		width: '100%',
-		[ theme.breakpoints.up( 'sm' ) ]: {
-			marginLeft: theme.spacing( 3 ),
-			width: 'auto',
-		},
-		backgroundColor: fade( theme.palette.common.white, 0.15 ),
-		'&:hover': {
-			backgroundColor: fade( theme.palette.common.white, 0.25 ),
-		},
+	title: {
+		flexGrow: '1',
+		fontFamily: 'inherit',
 	},
-	inputRoot: {
+	btn: {
 		color: 'inherit',
-	},
-	inputInput: {
-		padding: theme.spacing( 1, 1, 1, 0 ),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${ theme.spacing( 4 ) }px)`,
-		transition: theme.transitions.create( 'width' ),
-		width: '100%',
-		[ theme.breakpoints.up( 'md' ) ]: {
-			width: '20ch',
-		},
+		fontFamily: 'inherit',
+		fontSize: 'larger',
+		textTransform: 'capitalize',
 	},
 } ) );
 
@@ -53,26 +30,13 @@ function Navbar () {
 	const classes = useStyle();
 
 	return (
-		<AppBar className={ classes.appbar } position="sticky">
+		<AppBar className={ classes.appbar } color='transparent'>
 			<Toolbar>
-
-				<Typography variant="h4"> MELP </Typography>
-
-				<div className={ classes.search }>
-					
-					<InputBase
-						placeholder="Search"
-						classes={ {
-							root: classes.inputRoot,
-							input: classes.inputInput,
-						} }
-						inputProps={ { 'aria-label': 'search' } }
-					/>
-
-					<SearchIcon fontSize="large" />
-
-				</div>
-
+				<Typography className={classes.title} variant="h4"> 
+					Melp
+				</Typography>
+				<Button className={classes.btn}>home</Button>
+				<Button className={classes.btn}>map</Button>
 			</Toolbar>
 		</AppBar>
 	)
