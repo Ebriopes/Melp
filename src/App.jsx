beb50bca5d2f890da532f11addbfdc5dc5ba70cb
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-17 08:49:50 -0500

    Added context provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React		from 'react';
 import Home			from './Views/Home';
 import Navbar		from './Components/Navbar';
 import Background	from './Components/Background';
+import RestaurantContextProvider from './Contexts/RestaurantContext';
 
 function App () {
 	return (
-		<React.Fragment>
+		<RestaurantContextProvider>
 			<Background/>
 			<Navbar />
 			<Home />
-		</React.Fragment>
+		</RestaurantContextProvider>
 	);
 };
 
