0243a902c2d0abad181554c253b5c6137952de33
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-17 08:32:24 -0500

    Functions separated, change reduce for map

--- src/Components/ShowRestaurants.jsx
+++ src/Components/ShowRestaurants.jsx
@@ -1,62 +1,55 @@
-import React, { useEffect, useState }	from 'react';
-import { CircularProgress, makeStyles, Typography }	from '@material-ui/core';
-import RestaurantService 				from '../Services/RestaurantService';
-import RestaurantCard 					from './RestaurantCard';
-import Filter							from './Filter';
+import React, { useContext }	from 'react';
+import { RestaurantContext }	from '../Contexts/RestaurantContext';
+import useStyle 				from '../Styles/MaterialStyles';
+import RestaurantCard 			from './RestaurantCard';
+import Filter					from './Filter';
 
-const useStyle = makeStyles( () => ( {
-	progress: {
-		display: 'block',
-		color: 'coral',
-		margin: '30px auto',
+import { 
+	CircularProgress,
+	Typography 
+}	from '@material-ui/core';
+
+/* Function will show a response depend of data base state */
+const loading = ( baseData ) => {
+	if ( baseData === null || baseData === undefined ) {
+		
+		return <Typography variant='h2' align='center' color='error'>
+					Oh, no!<br/>
+					There isn't restaurants 😞
+				</Typography>
+
+	} else if ( baseData.length === 0 ){
+		return <CircularProgress size={80} thickness={5} className={style.load}/>
+	}else{
+		return renderRestaurants(baseData)
 	}
-} ) );
+};
 
-function Presentation () {
-	const [ data, setData ] = useState( [ ] );
-	const classes = useStyle();
-
-	const showRestaurants = ( baseData ) => {
-		return <React.Fragment>
-
-				{baseData.reduce( ( prev, restaurant ) =>
-					<> 
-						{prev} 
-						<RestaurantCard {...restaurant}/> 
-					</>,<></>)}
-
-				</React.Fragment>
-	};
-
-	const loading = ( baseData ) => {
-		if ( baseData === null ){
-
-			return <Typography variant='h2' align='center' color='error'>
-						Oh, no!<br/>
-						There isn't restaurants 😞
-					</Typography>
-
-		} else if ( baseData.length === 0 ){
-
-			return <CircularProgress size={80} thickness={5} className={classes.progress}/>
-
-		}else{
-			
-			return <Filter data={data} setData={setData} showR={showRestaurants}/>
-
-		}
-	};
+/* Function will render all cards */
+const renderRestaurants = ( baseData ) => {
 	
-	useEffect( () => {
-		RestaurantService()
-		.then( ( dataBase ) => setData( dataBase ) )
-	}, []);
+	/* Set the object styles for the cards */
+	const style = useStyle();
+	
+	return ( 
+		<div className={ style.cards }>
+			{baseData.map( 
+				( restaurant ) => 
+					<RestaurantCard {...restaurant} /> )}
+		</div>
+	)
+}
+
+function ShowRestaurants () {
+	/* Call the data base from Context */
+	const { restaurantData } = useContext(RestaurantContext);
 
 	return (
 		<React.Fragment>
-			{loading( data )}
+			<Filter/>
+			{loading( restaurantData )}
 		</React.Fragment>
 	)
 };
 
-export default Presentation;
+export default ShowRestaurants;
