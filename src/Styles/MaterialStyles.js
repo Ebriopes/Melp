eae7a8eccb2a48b83564070c0d86b43eb0bbffe8
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-17 06:23:36 -0500

    added style from Home

--- src/Styles/MaterialStyles.js
+++ src/Styles/MaterialStyles.js
@@ -2,14 +2,20 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyle = makeStyles(() =>({
 	bkgPage: {
+		maskImage:	'linear-gradient(to bottom, rgba(0,0,0,1) 70%, rgba(0,0,0,0.3) 85%, rgba(0,0,0,0))',
 		objectFit:	'cover',
+		position:	'absolute',
+		zIndex:		'-1',
 		height:		'100vh',
 		width:		'100vw',
-		zIndex:		'-1',
-		position:	'absolute',
-		maskImage:	'linear-gradient(to bottom, rgba(0,0,0,1) 70%, rgba(0,0,0,0.3) 85%, rgba(0,0,0,0))',
 	},
-
+	homeText: {
+		textShadow:	'0 0 8px #000',
+		textAlign:	'center',
+		margin:		'10vh auto',
+		color:		'floralwhite',
+	},
+	
 }))
 
 export default useStyle;
