f5ea2a40535eb4981d8388be1cde6639c45ea798
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-17 09:28:32 -0500

    Reduce code with context

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,83 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { RestaurantContext } from '../Contexts/RestaurantContext';
 
 import {
 	Button,
 	ButtonGroup,
-	makeStyles,
 } from '@material-ui/core';
+import useStyle from '../Styles/MaterialStyles';
 
-const useStyle = makeStyles( () => ( {
-	menu: {
-		display:		'flex',
-		width:			'fit-content',
-		marginLeft:		'auto',
-		marginRight:	'5px',
-		backgroundColor:'beige',
-	},
-	cards:{
-		display:	'flex',
-		flexWrap:	'wrap',
-	},
-	btn:{
-		backgroundColor: 'inherit',
-	},
-} ) );
-
-function Filter ( { data, setData, showR } ) {
-	const [ rating, setRating ] = useState( false );
-	const [ alpha, setAlpha ] = useState( false );
-	const classes = useStyle();
-
-	const sortRating = () => {
-
-		const sort = () => data.sort( ( a, b ) => b.rating - a.rating );
-
-		setRating( !rating );
-
-		rating ?
-			setData( sort() ) :
-			setData( sort().reverse() );
-		console.info( data )
-	};
-
-	const sortAlpha = () => {
-
-		const sort = () =>
-
-			data.sort( ( a, b ) => {
-				if ( a.name > b.name ) {
-
-					return -1;
-
-				} else if ( a.name < b.name ) {
-					
-					return 1;
-
-				} else {
-					
-					return 0;
-
-				}
-			} );
-
-		setAlpha( !alpha );
-
-		alpha ?
-			setData( sort() ) :
-			setData( sort().reverse() );
-	}
+function Filter ( ) {
+	const { sortAlpha, sortRating } = useContext(RestaurantContext);
+	const style = useStyle();
 
 	return (
-		<div>
-			<ButtonGroup variant='contained' color='inherit' className={ classes.menu }>
-				<Button onClick={ sortRating } className={classes.btn}>Rating</Button>
-				<Button onClick={ sortAlpha } className={classes.btn}>Name</Button>
-			</ButtonGroup>
-			<div className={classes.cards}>
-				{showR(data)}
-			</div>
-		</div>
-	)
-}
+		<ButtonGroup variant='contained' className={ style.filterGroup }>
+			<Button onClick={ sortAlpha } className={ style.filterBtn }>
+				Name
+			</Button>
+			<Button onClick={ sortRating } className={ style.filterBtn }>
+				Rating
+			</Button>
+		</ButtonGroup>
+	);
+};
 
 export default Filter;
