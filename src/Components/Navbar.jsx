b163fb4d70ef613d8a23027686f957b5ef2d4018
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-20 16:36:00 -0500

    Seet style in file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,39 @@ import {
 	Typography,
 } from '@material-ui/core';
 
-import useStyle from '../Styles/MaterialStyles';
+/* import useStyle from '../Styles/MaterialStyles';
+ */
+
+const useStyle = makeStyles( ( theme ) => ( {
+	bkgPage: {
+		maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 70%, rgba(0,0,0,0.3) 85%, rgba(0,0,0,0))',
+		objectFit: 'cover',
+		position: 'absolute',
+		zIndex: '-1',
+		height: '100vh',
+		width: '100%',
+	},
+	navBar: {
+		position: 'static',
+		color: 'coral',
+		boxShadow: 'none',
+		fontFamily: 'Lobster',
+		textShadow: '0 0 8px #000',
+	},
+	navTitle: {
+		flexGrow: '1',
+		fontFamily: 'inherit',
+	},
+	navLink: {
+		color: 'inherit',
+		margin: '0 10px',
+		fontSize: 'larger',
+		fontFamily: 'inherit',
+		textShadow: 'inherit',
+		textTransform: 'capitalize',
+		textDecoration: 'none',
+	},
+} ) )
 
 function Navbar () {
 
