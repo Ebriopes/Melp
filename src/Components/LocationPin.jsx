560e1513d2cb27978aae8e46ecccb9fe996080f8
Ebriopes <antonio.vargasrosales@gmail.com>
2020-09-19 20:38:16 -0500

    Change icon

--- src/Components/LocationPin.jsx
+++ src/Components/LocationPin.jsx
@@ -1,10 +1,10 @@
-import { PinDrop }	from '@material-ui/icons';
+import { Room }	from '@material-ui/icons';
 import React 		from 'react';
 
 function LocationPin ( { text } ) {
 	return (
 		<div>
-			<PinDrop fontSize='large' style={ { color: 'darkcyan' } }/>
+			<Room fontSize='large' style={ { color: 'darkcyan' } }/>
 			{/* <h1>{ text }</h1> */}
 		</div>
 	)
